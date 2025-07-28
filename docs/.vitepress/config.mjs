9494302438125659a9ebdcd28d9fbca5dde88fef
weixin_45501559 <weixin_45501559@noreply.gitcode.com>
2025-07-28 19:52:47 +0800

    更改目录

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -31,8 +31,36 @@ export default defineConfig({
     ],
 
     sidebar: [
-      
-    ],
+  {
+    text: 'Java',
+    collapsed: false, // 默认展开
+    items: [
+      { text: 'Java基础', link: '/java/BaseJava' },
+      { 
+        text: '数据结构', 
+        collapsed: true, // 子目录是否折叠
+        items: [
+          { text: '概述', link: '/java/java-data-structure/' }, // index.md
+          { text: '链表', link: '/java/java-data-structure/LinkedList' },
+          { text: '动态数组', link: '/java/java-data-structure/ArrayList' },
+          { text: '哈希表', link: '/java/java-data-structure/HashMap' },
+          { text: '树结构', link: '/java/java-data-structure/Tree' },
+          { text: '队列', link: '/java/java-data-structure/queue' },
+        ]
+      },
+      { text: 'SpringBoot', link: '/java/SpringBoot' },
+      {text: 'JavaIO',link: '/java/JavaIO'},
+      {text: 'IO模型',link: '/java/IOModel'},
+      {
+        text: 'Netty',
+        collapsed: true, // 子目录是否折叠
+        items: [
+          {text: 'Netty基础',link: '/java/Netty/Netty基础'},
+        ]
+      }
+    ]
+  }
+],
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
